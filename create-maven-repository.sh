1042f668907ba3b725780319c8721cc2784a5953
Uli Heller <uli.heller@daemons-point.com>
2025-01-10 18:17:49 +0100

    Creation of maven-repository

--- create-maven-repository.sh
+++ create-maven-repository.sh
@@ -6,6 +6,6 @@ D="$(realpath "${D}")"
 MAVEN_REPOSITORY_FOLDER="${D}/maven-repository"
 MAVEN_REPOSITORY_URL="file://${MAVEN_REPOSITORY_FOLDER}"
 
-#( cd hello-world && "${D}/gradlew" --stacktrace -PmavenRepositoryUrl="${MAVEN_REPOSITORY_URL}" publish )
-#"${D}/gradlew" -b bye-moon/build.gradle -PmavenRepositoryUrl="${MAVEN_REPOSITORY_URL}" publish
+( cd hello-world && "${D}/gradlew" --stacktrace -PmavenRepositoryUrl="${MAVEN_REPOSITORY_URL}" publish )
+( cd bye-moon && "${D}/gradlew" --stacktrace -PmavenRepositoryUrl="${MAVEN_REPOSITORY_URL}" publish )
 ( cd maybe-mars && "${D}/gradlew" --stacktrace -PmavenRepositoryUrl="${MAVEN_REPOSITORY_URL}" publish )
